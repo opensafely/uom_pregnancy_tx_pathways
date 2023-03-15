ea464f8add8276a3e92e9ab8ccd9374d8406453b
v1kki213 <victoria.palin@manchester.ac.uk>
2023-03-15 10:45:13 +0000

    Update del_code_frequency_with_names.R

--- analysis/del_code_frequency_with_names.R
+++ analysis/del_code_frequency_with_names.R
@@ -11,7 +11,7 @@ df_1 <- read_csv(
 #load frequency of codes
 df_2 <- read_csv(
   here::here("output", "table_del_codes.csv"),
-  col_types = cols(code = col_number(),frequency = col_number())
+  col_types = cols(Var1 = col_number(),Freq = col_number())
 )
 
 #changed integer to number above - got error
@@ -19,13 +19,7 @@ df_2 <- read_csv(
 #rename first column in frequency table to match
 colnames(df_2)[1]<-"code"
 
-#return which codes are in both files
-#df1$code %in% df2$code
-
+#merge by code number to return full description of codes which are in both files
 df_3<-merge(df_2, df_1, by="code")
 
-#do we need to add term to this
-table_del_codes_with_names <- as.data.frame(table(df_3$code)) 
-table_del_codes_with_names <- table_del_codes_with_names[order(-table_del_codes_with_names$Freq),]
-
-write_csv(table_del_codes_with_names, here::here("output","table_del_codes_with_names.csv"))
+write_csv(df_3, here::here("output","table_del_codes_with_names.csv"))
