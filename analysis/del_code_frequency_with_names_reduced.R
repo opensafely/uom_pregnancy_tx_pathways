6ce76ccb96f4a38cfcab1ef761fe221c510a942e
djlhayes <dexter.hayes@manchester.ac.uk>
2023-03-17 11:56:36 +0000

    reduced codelist added
    table_del_codes_with_names_reduced.csv added, freq >5 only

---
+++ analysis/del_code_frequency_with_names_reduced.R
@@ -0,0 +1,12 @@
+library('tidyverse')
+
+options(scipen=99999)
+
+df_1 <- read_csv(
+  here::here("output", "table_del_codes_with_names.csv"),
+  col_types = cols(code = col_number(),term = col_character())
+)
+
+df_2=df_1%>% filter(Freq > 5)
+
+write_csv(df_2, here::here("output","table_del_codes_with_names_reduced.csv"))
