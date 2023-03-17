6886bec05db470bbc2a3a93e6b9bac0c89e03f8f
djlhayes <dexter.hayes@manchester.ac.uk>
2023-03-17 12:26:57 +0000

    adding rounding
    ignore previous commit - did wrong thing

--- analysis/del_code_frequency_with_names_reduced.R
+++ analysis/del_code_frequency_with_names_reduced.R
@@ -7,6 +7,8 @@ df_1 <- read_csv(
   col_types = cols(code = col_number(),term = col_character())
 )
 
-df_2=df_1%>% filter(Freq > 5)
+df_2=df_1%>% mutate(
+Freq = case_when( Freq > 5 ~ ),
+)
 
 write_csv(df_2, here::here("output","table_del_codes_with_names_reduced.csv"))
