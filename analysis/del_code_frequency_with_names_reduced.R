ac965c3006b6ce2df9693b3731437b1b4f987ee5
djlhayes <dexter.hayes@manchester.ac.uk>
2023-03-17 13:05:30 +0000

    reduced.csv fixed
    del_code_freq_with_names_reduced.csv added, counts under 6 suppressed

--- analysis/del_code_frequency_with_names_reduced.R
+++ analysis/del_code_frequency_with_names_reduced.R
@@ -7,8 +7,16 @@ df_1 <- read_csv(
   col_types = cols(code = col_number(),term = col_character())
 )
 
-df_2=df_1%>% mutate(
-Freq = case_when( Freq > 5 ~ ),
-)
+#changes counts under 6 to "n" - what is best notation
+#have checked this works in R 
+df_2 <- df_1 %>% mutate(Freq = case_when(Freq < 6 ~ "n"))
+
+## example code
+#mutate
+#    n = case_when(n>7 ~ n),
+#        n = round(n/5)*5,
+#    total = case_when (total<7 ~ total),
+#        total = round(total/5)*5,
+#    pcent_total = n/total*100)
 
 write_csv(df_2, here::here("output","table_del_codes_with_names_reduced.csv"))
