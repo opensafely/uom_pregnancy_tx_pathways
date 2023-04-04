feeebc926e37918a17137cd36637eb0da3d128a4
djlhayes <dexter.hayes@manchester.ac.uk>
2023-04-04 15:04:26 +0100

    Update pn_code_frequency.R

--- analysis/pn_code_frequency.R
+++ analysis/pn_code_frequency.R
@@ -3,20 +3,24 @@
 # 2. write code to combine files
 # 3. write code for counts - table and/or plot
 
-# from notes     df2<-df1%>%group_by(date)     
-# rbind (df1, df2)
-# ^ do this for all measures by month?
+
 # then sort/group by patient id?
 # what do we want - no of codes per person per month
+# average isnt useful - number of zeroes
 
-#example? https://github.com/opensafely/amr-uom-brit/blob/service_evaluation/analysis/plot/ab_recorded_indication_plot_incident.R 
+#csvFiles=list.files(pattern = "input", full.names = FALSE)
 
 library('tidyverse')
 
-# df_input <- read_csv(
-#   here::here("output", "measures", "input_measures_2019-01-01.csv.gz"),
-# input_measures or input_monthly?
-# OR
+dfmonths=list.files(pattern = "input", full.names = FALSE) %>%
+     lapply(read.csv, stringsAsFactors=F) %>%
+     bind_rows()
+
+View(dfmonths)
+colnames(dfmonths)
+
+#still need to group by patient ID?
+
 #   this is from del code table
 #   here:: ("output", "measures", "input_*.csv.gz"),
 #    and then group by month and find mean per person? 
@@ -27,9 +31,9 @@ library('tidyverse')
 
 #have tested code below on measures file for one month
 
-df_input <- df_input %>% filter(pn8wk_code_number >0)
+#dfmonths <- dfmonths %>% filter(pn8wk_code_number >0)
 
- plot_pn8wk_code_frequency <- ggplot(data=df_input, aes(df_input$pn8wk_code_number)) + 
+ plot_pn8wk_code_frequency <- ggplot(data=dfmonths, aes(dfmonths$pn8wk_code_number)) + 
  geom_histogram() +
  labs (title = "Pn 8wk Code Distribution",
  x = "No. of postnatal codes")
@@ -40,7 +44,7 @@ df_input <- df_input %>% filter(pn8wk_code_number >0)
  )
 
 #table for no. of codes by frequency
-table_pn_codes <- as.data.frame(table(df_input$delivery_code)) 
+table_pn_codes <- as.data.frame(table(dfmonths$delivery_code)) 
 
 #table above sorted by frequency, rename var1?
 table_pn_codes <- table_pn_codes[order(-table_del_codes$Freq),]
