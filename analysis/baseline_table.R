779e439bc56bfaee18a0a087ab79a55dcfa5a1a9
djlhayes <dexter.hayes@manchester.ac.uk>
2023-04-13 10:53:22 +0100

    Update baseline_table.R
    moved variables to top and split dfs before selecting one row for each patient id

--- analysis/baseline_table.R
+++ analysis/baseline_table.R
@@ -16,67 +16,68 @@ df$delivery_code_date<-as.Date(df$delivery_code_date)
 ##dfmonths$delcode<-as.Date(dfmonths$delivery_code_date)
 ##dfmonths$cal_month<-month(dfmonths$delcode)
 ##dfmonths$cal_year<-year(dfmonths$delcode)
-##create df_date variable as MM-YYYY
 ##dfmonths$df_date<-paste0(dfmonths$cal_month, "-", dfmonths$cal_year)
 
-### start from here
-# 1. group by patient ID
-# 2. filter del codes >0
-# 3. arrange by descending del_code _date
-# 4. keep first row per group
-# 5. define/check/add rules for variables
-# 6. create overall/before/after dfs
-# 7. summary tables
 
-## 1. group by patient ID and sort in ascending order
-df2<-df%>%group_by(patient_id)
-
-## 2. filter del codes >0
-df3<-df2%>% filter(delivery_code_present > 0)
-
-## 3. arrange by patient ID then del code date
-#arrange(data, by_group = TRUE)
-#or just arrange by both? line below works
-df4<-df3%>%arrange(patient_id, desc(delivery_code_date))
-
-## 4. filter by first row to get last date in study period
-df5<-df4%>% filter(row_number()==1)
-
-## old code - do we need to do this?
-#create df_sum - shows no of del codes by date and patient ID 
-#df_sum<-df%>%group_by(patient_id, delivery_code_date)%>%summarise(delivery_code_present)
-
-## 5. variables
-# we have age (numeric) and age cat (char)
-# eth as 1-6, bmi as a number,
-# imd as 1-5, number of del codes?
+## 1. Define/clean variables before splitting dfs
 
 # bmi - numeric
 # remove bmi outliers - this replaces <8 or >50 with NA
-df5$bmi <- ifelse(df5$bmi <8 | df5$bmi>50, NA, df5$bmi)
-# do we want to add categories?
-# this works in R, just takes a while to run
-df5<-df5%>%mutate(bmi_cat = case_when(is.na(bmi) ~ "Unknown",
+df$bmi <- ifelse(df$bmi <8 | df$bmi>50, NA, df$bmi)
+df <- df %>% mutate(bmi_cat = case_when(is.na(bmi) ~ "Unknown",
                                     bmi>=8 & bmi< 18.5 ~ "Underweight",
                                     bmi>=18.5 & bmi<=24.9 ~ "Healthy weight",
                                     bmi>24.9 & bmi<=29.9 ~ "Overweight",
                                     bmi>29.9 ~"Obese"))
-df5$bmi_cat<-as.factor(df5$bmi_cat)
+df$bmi_cat <- as.factor(df$bmi_cat)
 
 # imd as a factor - do these have/need labels?
-df5$imd<-as.factor(df5$imd)
+df$imd <- as.factor(df$imd)
 
 ## ethnicity
 # replace NA with 6 ("unknown") then convert to factor
-# works in R, also takes a while 
-df5$ethnicity=ifelse(is.na(df5$ethnicity),"6",df5$ethnicity)
-df5<-df5%>% mutate(ethnicity_6 = case_when(ethnicity == 1 ~ "White",
+df$ethnicity=ifelse(is.na(df$ethnicity),"6",df$ethnicity)
+df <- df %>% mutate(ethnicity_6 = case_when(ethnicity == 1 ~ "White",
                                  ethnicity == 2  ~ "Mixed",
                                  ethnicity == 3  ~ "South Asian",
                                  ethnicity == 4  ~ "Black",
                                  ethnicity == 5  ~ "Other",
                                  ethnicity == 6   ~ "Unknown"))
-df5$ethnicity_6 <- as.factor(df5$ethnicity_6)
+df$ethnicity_6 <- as.factor(df$ethnicity_6)
+
+#Create before/after pandemic dfs, keep overall
+# use del_code_date to split?
+# check dates and date format - before/after first lockdown?
+df_overall <- df
+df_before <- df %>% filter(del_code_date < "2020-03-01") 
+df_after <- df %>% filter(del_code_date >= "2020-03-01") 
+
+#dates from plots
+#  as.Date("2021-01-01"),xmax = as.Date("2021-04-01")
+#  as.Date("2020-11-01"),xmax = as.Date("2020-12-01")
+#  as.Date("2020-03-01"),xmax = as.Date("2020-06-01")
+
+## 2. group by patient ID and sort in ascending order
+df_overall2 <- df %>% group_by(patient_id)
+df_before2 <- df_before %>% group_by(patient_id)
+df_after2 <- df_after %>% group_by(patient_id)
+
+## 3. filter del codes >0
+df_overall3 <- df_overall2 %>% filter(delivery_code_present > 0)
+df_before3 <- df_before2 %>% filter(delivery_code_present > 0)
+df_after3 <- df_after2 %>% filter(delivery_code_present > 0)
+
+## 4. arrange by patient ID then del code date
+# then filter by first row to get last date in study period
+#arrange(data, by_group = TRUE)
+#or just arrange by both? line below works
+df_overall4 <- df_overall3 %>% arrange(patient_id, desc(delivery_code_date)) %>% filter(row_number()==1)
+df_before4 <- df_before3 %>% arrange(patient_id, desc(delivery_code_date)) %>% filter(row_number()==1)
+df_after4 <- df_after3 %>% arrange(patient_id, desc(delivery_code_date)) %>% filter(row_number()==1)
+
+## old code - do we need to do this?
+#create df_sum - shows no of del codes by date and patient ID 
+#df_sum<-df%>%group_by(patient_id, delivery_code_date)%>%summarise(delivery_code_present)
 
 ## code for overall counts - use for paper to get number of practices etc
 # think about which vars we need this for
@@ -84,23 +85,11 @@ df5$ethnicity_6 <- as.factor(df5$ethnicity_6)
 # write_csv(overall_counts, here::here("output", "overall_counts_blt_2020.csv"))
 # rm(overall_counts) 
 
-## 6. Create before/after pandemic dfs, keep overall
-# use del_code_date to split?
-# check dates and date format - before/after first lockdown?
-df_overall<- df5
-df_before <- df_overall%>%filter(del_code_date < "2020-03-01") 
-df_after <- df_overall%>%filter(del_code_date >= "2020-03-01") 
-
-#dates from plots
-#  as.Date("2021-01-01"),xmax = as.Date("2021-04-01")
-#  as.Date("2020-11-01"),xmax = as.Date("2020-12-01")
-#  as.Date("2020-03-01"),xmax = as.Date("2020-06-01")
-
-## 7. Create summary table
+## 5. Create summary table
 
 # select variables for the baseline table
 # bmi and bmi cat? are we looking at averages?
-bltab_vars <- df_overall %>% select(age, age_cat, bmi, bmi_cat, ethnicity_6, del_code_number) 
+bltab_vars <- df_overall %>% select(age, age_cat, bmi, bmi_cat, del_code_number, ethnicity_6, imd) 
 
 # # columns for baseline table
 colsfortab <- colnames(bltab_vars)
