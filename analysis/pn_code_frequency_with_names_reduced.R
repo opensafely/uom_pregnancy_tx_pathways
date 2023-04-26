107b7d0738cb034eae611e3f374448a258174650
djlhayes <dexter.hayes@manchester.ac.uk>
2023-04-26 15:45:52 +0100

    reduced table added
    run on server

---
+++ analysis/pn_code_frequency_with_names_reduced.R
@@ -0,0 +1,20 @@
+library('tidyverse')
+
+options(scipen=99999)
+
+df1 <- read_csv(
+  here::here("output", "table_pn_codes_with_names.csv"),
+  col_types = cols(code = col_number(),Freq=col_number(), term = col_character())
+)
+
+#changes counts under 6 to "[REDACTED]"
+df2 <- df1 
+df2$Freq <- ifelse(df2$Freq <= 7, "[REDACTED]", df2$Freq)
+df2$Freq <- as.numeric(df2$Freq)
+
+#rounding to nearest 5
+df3<-df2
+df3$Freq <- round(df3$Freq/5)*5
+
+write_csv(df3, here::here("output","table_pn_codes_with_names_reduced.csv"))
+
