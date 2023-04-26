f23c7caa9e482317264a0b084ed01775b160d454
djlhayes <dexter.hayes@manchester.ac.uk>
2023-04-26 12:03:59 +0100

    pn code table added
    ready to run on server - do we need redacted table too?

--- analysis/pn_code_frequency.R
+++ analysis/pn_code_frequency.R
@@ -2,7 +2,7 @@ library('tidyverse')
 
 df_input <- read_csv(
   here::here("output", "input.csv"),
-  col_types = cols(patient_id = col_integer(),delivery_code_number = col_number(), postnatal_8wk_code_present = col_character())
+  col_types = cols(patient_id = col_integer(),delivery_code_number = col_number(), postnatal_8wk_code_present = col_character(), postnatal_code = col_number())
 )
 
 df_input <- df_input %>% filter(postnatal_8wk_code_present >0)
@@ -10,19 +10,19 @@ df_input <- df_input %>% filter(postnatal_8wk_code_present >0)
 #might need library('dplyr')
 
 
-plot_delivery_code_frequency <- ggplot(data=df_input, aes(df_input$delivery_code_number)) + 
-geom_histogram() +
-labs (title = "Delivery Code Distribution",
-x = "No. of delivery codes")
+# plot_delivery_code_frequency <- ggplot(data=df_input, aes(df_input$delivery_code_number)) + 
+# geom_histogram() +
+# labs (title = "Delivery Code Distribution",
+# x = "No. of delivery codes")
 
 
-ggsave(
-  plot= plot_delivery_code_frequency,
-  filename="del_code_histogram.jpeg", path=here::here("output"),
-)
+# ggsave(
+#   plot= plot_delivery_code_frequency,
+#   filename="del_code_histogram.jpeg", path=here::here("output"),
+# )
 
 
-table_del_codes <- as.data.frame(table(df_input$delivery_code)) 
-table_del_codes <- table_del_codes[order(-table_del_codes$Freq),]
+table_pn_codes <- as.data.frame(table(df_input$postnatal_code)) 
+table_pn_codes <- table_pn_codes[order(-table_pn_codes$Freq),]
 
-write_csv(table_del_codes, here::here("output","table_del_codes.csv"))
+write_csv(table_pn_codes, here::here("output","table_pn_codes.csv"))
--- analysis/pn_code_frequency_with_names.R
+++ analysis/pn_code_frequency_with_names.R
@@ -10,7 +10,7 @@ df_1 <- read_csv(
 
 #load frequency of codes
 df_2 <- read_csv(
-  here::here("output", "table_del_codes.csv"),
+  here::here("output", "table_pn_codes.csv"),
   col_types = cols(Var1 = col_number(),Freq = col_number())
 )
 
@@ -22,4 +22,4 @@ df_3<-merge(df_2, df_1, by="code")
 df_3 <- df_3[order(-df_3$Freq),]
 
 
-write_csv(df_3, here::here("output","table_del_codes_with_names.csv"))
+write_csv(df_3, here::here("output","table_pn_codes_with_names.csv"))
