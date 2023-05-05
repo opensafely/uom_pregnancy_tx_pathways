439d0e05d87e384740972a869ae3fe537f6e9d9f
djlhayes <dexter.hayes@manchester.ac.uk>
2023-05-05 14:24:27 +0100

    Update pn_check_ITS_plot.R
    changed to load individual measures files + combine, look at count var

--- analysis/pn_check_ITS_plot.R
+++ analysis/pn_check_ITS_plot.R
@@ -7,15 +7,24 @@ library("MASS")
 #library("gtsummary")
 
 ## Import data
-df = read.csv(here::here("output", "measures", "measure_postnatal_check_rate.csv")),
+#df = read.csv(here::here("output", "measures", "measure_postnatal_check_rate.csv")),
 
-# for measures overall,
-# or do we want to bind age_cat, eth, etc files
+df1 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_age_cat.csv")),
+df1$measure="age_cat"
+df2 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_ethnicity.csv")),
+df2$measure="ethnicity"
+df3 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_imd.csv")),
+df3$measure="imd"
+df4 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_practice.csv")),
+df4$measure="practice"
+
+df=rbind(df1,df2,df3,df4)
 
 delivery_code_present  = col_double(),
 postnatal_8wk_code_present = col_double(),
 population  = col_number(),
 value = col_number(),
+measure = col_character(),
 
 df$date <- as.Date(df$date)
 #df$month= format(df$date,"%m")
@@ -38,14 +47,14 @@ df=df%>% filter(covid==1 | covid==2)
 df$covid= recode(df$covid, '1'="0", '2'="1")
 df$covid <- factor(df$covid, levels=c("0","1"))
 
-## creates time.since
-# as time since start of obs period
+## creates time.since as time since start of obs period
 # all covid = 0 are 0
 # covid = 1 are 1 to 476 (each row a new no) - is this right?
 df=df%>% group_by(covid)%>%mutate(time.since=1:n())
 df$time.since <- ifelse(df$covid==0,0,df$time.since)
 
-# do we need this?
+## creates value as 1-percentage, what is our equivalent? 
+## are these nom/denom?
 # df$value=1-df$percentage
 # df$count= df$total-df$count
 
@@ -54,6 +63,29 @@ df$time.since <- ifelse(df$covid==0,0,df$time.since)
 # covid (binary)
 # time.since (months since covid)
 
-## next steps
-# filter by age_cat? to make split dfs
-#this currently loads in overall measures file
+df_overall=df
+# these are all already filtered by del code >0?
+## split data
+df1=df_overall%>%filter(measure=="age_cat")
+df2=df_overall%>%filter(measure=="ethnicity")
+df3=df_overall%>%filter(measure=="imd")
+df4=df_overall%>%filter(measure=="practice")
+
+## should now have a df for each measure again -- check
+## need to define/check count var / our equivalent
+
+# 1. overall?
+
+# 2.  age_cat
+m2.1 <- glm.nb(count~ offset(log(total)) + covid + times + time.since  , data = df1)
+
+# 3.  ethnicity
+m3.1 <- glm.nb(count~ offset(log(total))+ covid + times + time.since  , data = df2)
+
+# 3.  imd
+m4.1 <- glm.nb(count~ offset(log(total)) + covid + times + time.since  , data = df3)
+
+# 5.  practice
+m5.1 <- glm.nb(count~ offset(log(total))+  covid + times + time.since  , data = df4)
+
+
