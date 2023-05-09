0d5bab0910532c431751a497b8032705c16036c1
djlhayes <dexter.hayes@manchester.ac.uk>
2023-05-09 14:16:20 +0100

    overall plot
    plot works using overall data, need to figure out how to bind measure files for age_cat, eth etc while assigning numbers to individual months rather than rows

--- analysis/pn_check_ITS_plot.R
+++ analysis/pn_check_ITS_plot.R
@@ -7,18 +7,29 @@ library("MASS")
 #library("gtsummary")
 
 ## Import data
-#df = read.csv(here::here("output", "measures", "measure_postnatal_check_rate.csv")),
+df = read.csv(here::here("output", "measures", "measure_postnatal_check_rate.csv")),
 
-df1 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_age_cat.csv")),
-df1$measure="age_cat"
-df2 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_ethnicity.csv")),
-df2$measure="ethnicity"
-df3 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_imd.csv")),
-df3$measure="imd"
-df4 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_practice.csv")),
-df4$measure="practice"
+## add measure column and rename so we can bind by row
+# df1 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_age_cat.csv")),
+# df1$measure="age_cat"
+# df1<-df1%>%rename(var1=age_cat)
 
-df=rbind(df1,df2,df3,df4)
+# df2 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_ethnicity.csv")),
+# df2$measure="ethnicity"
+# df2<-df2%>%rename(var1=ethnicity)
+
+# df3 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_imd.csv")),
+# df3$measure="imd"
+# df3<-df3%>%rename(var1=imd)
+
+# df4 = read.csv(here::here("output", "measures", "measure_postnatal_check_rate_by_practice.csv")),
+# df4$measure="practice"
+# df4<-df4%>%rename(var1=practice)
+
+# df=rbind(df1,df2,df3,df4)
+
+filter by del code present?
+df<-df%>%filter(delivery_code_present>0)
 
 delivery_code_present  = col_double(),
 postnatal_8wk_code_present = col_double(),
@@ -27,7 +38,7 @@ value = col_number(),
 measure = col_character(),
 
 df$date <- as.Date(df$date)
-#df$month= format(df$date,"%m")
+df$month= format(df$date,"%m")
 
 # creates time since start of study period as 1-52
 df$times <- as.numeric(as.factor(df$date))
@@ -38,8 +49,7 @@ breaks <- c(as.Date("2019-01-01"), as.Date("2020-03-01"), max(df$date))
 
 df=df%>%mutate(covid=cut(date,breaks,labels = 1:2))
 
-## gives a covid column with 1-2
-## as we dont want to exclude any months?
+## gives a covid column with 1-2 (no exclusions?)
 # 1 = before march 2020
 # 2 = march 2020 onwards
 
@@ -47,45 +57,50 @@ df=df%>% filter(covid==1 | covid==2)
 df$covid= recode(df$covid, '1'="0", '2'="1")
 df$covid <- factor(df$covid, levels=c("0","1"))
 
-## creates time.since as time since start of obs period
-# all covid = 0 are 0
-# covid = 1 are 1 to 476 (each row a new no) - is this right?
+## this doesnt work with multiple rows for each month?
+## times works for months
 df=df%>% group_by(covid)%>%mutate(time.since=1:n())
 df$time.since <- ifelse(df$covid==0,0,df$time.since)
 
-## creates value as 1-percentage, what is our equivalent? 
-## are these nom/denom?
+## from other repo - do we need?
 # df$value=1-df$percentage
 # df$count= df$total-df$count
 
 ## new vars so far
-# times (months since start of study)
-# covid (binary)
-# time.since (months since covid)
+# times (months since start of study) = T
+# covid (binary) = D
+# time.since (months since covid) = P
+# our outcome var is rate (value in .csv) = Y
 
 df_overall=df
-# these are all already filtered by del code >0?
 ## split data
-df1=df_overall%>%filter(measure=="age_cat")
-df2=df_overall%>%filter(measure=="ethnicity")
-df3=df_overall%>%filter(measure=="imd")
-df4=df_overall%>%filter(measure=="practice")
+#df0=df_overall
+# df1=df_overall%>%filter(measure=="age_cat")
+# df2=df_overall%>%filter(measure=="ethnicity")
+# df3=df_overall%>%filter(measure=="imd")
+# df4=df_overall%>%filter(measure=="practice")
 
-## should now have a df for each measure again -- check
-## need to define/check count var / our equivalent
 
+##warnings for non-integers, is this why we transform data?
 # 1. overall?
+m1.0 <- glm.nb(value~ offset(log(population)) + covid + times + time.since  , data = df1)
 
-# 2.  age_cat
-m2.1 <- glm.nb(count~ offset(log(total)) + covid + times + time.since  , data = df1)
+# # 2.  age_cat
+# m2.1 <- glm.nb(count~ offset(log(total)) + covid + times + time.since  , data = df1)
 
-# 3.  ethnicity
-m3.1 <- glm.nb(count~ offset(log(total))+ covid + times + time.since  , data = df2)
+# # 3.  ethnicity
+# m3.1 <- glm.nb(count~ offset(log(total))+ covid + times + time.since  , data = df2)
 
-# 3.  imd
-m4.1 <- glm.nb(count~ offset(log(total)) + covid + times + time.since  , data = df3)
+# # 3.  imd
+# m4.1 <- glm.nb(count~ offset(log(total)) + covid + times + time.since  , data = df3)
 
-# 5.  practice
-m5.1 <- glm.nb(count~ offset(log(total))+  covid + times + time.since  , data = df4)
+# # 5.  practice
+# m5.1 <- glm.nb(count~ offset(log(total))+  covid + times + time.since  , data = df4)
 
 
+(est1.0 <- cbind(Estimate = coef(m1.0), confint(m1.0)))
+
+exp1.0=exp(est1.0)
+
+##add labels etc
+ggplot(df_overall, aes(x=date, y=value, group=covid))+  theme_bw()+ annotate(geom = "rect", xmin = as.Date("2019-12-01"),xmax = as.Date("2020-04-01"),ymin = -Inf, ymax = Inf,fill="grey60", alpha=0.5)+ annotate(geom = "rect", xmin = as.Date("2020-04-01"),xmax = as.Date("2021-12-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+ geom_point(shape=4)+ geom_smooth(color="black",se = FALSE)+ scale_y_continuous(labels = scales::percent)+ scale_x_date(date_breaks = "1 month",date_labels =  "%Y-%m")+ theme(axis.text.x = element_text(angle = 60,hjust=1), legend.position = "bottom",legend.title =element_blank())+ labs(title = "", x = "", y = "")
