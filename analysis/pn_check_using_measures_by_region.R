dca4df460f0fbc35022cc26976e861255ac66b2e
djlhayes <dexter.hayes@manchester.ac.uk>
2023-06-19 14:54:51 +0100

    adding plots

--- analysis/pn_check_using_measures_by_region.R
+++ analysis/pn_check_using_measures_by_region.R
@@ -9,9 +9,11 @@ rm(list=ls())
 #setwd(here::here("output", "measures"))
 
 df <- read_csv(
-  here::here("output", "measures", "measure_postnatal_check_rate.csv"),
-
+  here::here("output", "measures", "measure_postnatal_check_rate_by_region.csv"),
   col_types = cols_only(
+
+    #Identifier
+    region = col_factor(),
     
     # Outcomes
     delivery_code_present  = col_double(),
@@ -29,10 +31,6 @@ df <- read_csv(
 
 ## remove rows where delivery_code_present == 0 (group_by var in measures)
 df=df%>% filter(delivery_code_present > 0)
-
-## below - if we are looking at +84 days do we want to remove last 2/3 months?
-## then for 6/8 weeks change this? and var in #outcomes above
-## set last.date to max(df$date) - 3 months then filter?
 
 # remove last month data
 df$date <- as.Date(df$date)
@@ -63,26 +61,13 @@ df2$population_rounded<-round(df2$population_redacted/5)*5
 df2$value_r=df2$postnatal_8wk_code_present_rounded/df2$population_rounded
 df_plot=df2 %>% filter(!is.na(value_r))
 
-# #changes counts under 6 to "[REDACTED]"
-# df2 <- df 
-# df2$postnatal_8wk_code_present_redacted <- ifelse(df2$postnatal_8wk_code_present <= 7, "NA", df2$postnatal_8wk_code_present)
-# df2$postnatal_8wk_code_present_redacted <- as.numeric(df2$postnatal_8wk_code_present_redacted)
-
-# df2$population_redacted <- ifelse(df2$population <= 7, "NA", df2$population)
-# df2$population_redacted <- as.numeric(df2$population_redacted)
-
-# #rounding to nearest 5
-# df2$postnatal_8wk_code_present_rounded<-round(df2$postnatal_8wk_code_present_redacted/5)*5
-# df2$population_rounded<-round(df2$population_redacted/5)*5
-
-# #create value_r based on rounded/redacted values
-# df2$value_r<-df2$postnatal_8wk_code_present_rounded/df2$population_rounded
-
 ### get monthly rate per 1000 patients
 df_monrate <- df_plot%>% group_by(cal_mon, cal_year) %>%
   mutate(pn_rate_1000 = value_r*1000) 
 
+# create dataframe without NA 
 df_gaps=df_monrate%>%filter(!is.na(postnatal_8wk_code_present_rounded))
+
 
 # df_mean <- df_monrate %>% group_by(cal_mon, cal_year) %>%
 #   mutate(meanrate = mean(pn_rate_1000,na.rm=TRUE),
@@ -91,17 +76,16 @@ df_gaps=df_monrate%>%filter(!is.na(postnatal_8wk_code_present_rounded))
 #          ninefive= quantile(pn_rate_1000, na.rm=TRUE, c(0.95)),
 #          five=quantile(pn_rate_1000, na.rm=TRUE, c(0.05)))
 
-
-plot_pn_rate <- ggplot(df_gaps, aes(x=date))+
-  geom_line(aes(y=pn_rate_1000),color="steelblue")+
-  geom_point(aes(y=pn_rate_1000),color="steelblue")+
+plot_pn_rate <- ggplot(df_gaps, aes(x=date, group=region, color=region))+
+  geom_line(aes(y=pn_rate_1000))+
+  #geom_line(data=df_gaps, linetype="dashed", aes(color+region))+ geom_point(aes(y=pn_rate_1000))+
   scale_x_date(date_labels = "%m-%Y", date_breaks = "1 month")+
   theme(axis.text.x=element_text(angle=60,hjust=1))+
   labs(
     title = "Rate of PN checks by month",
     subtitle = paste(first_mon,"-",last_mon),
     #caption = paste("Data from approximately", num_uniq_prac,"TPP Practices"),
-    x = "",
+    x = "Month",
     y = "Rate of PN checks per 1000 registered patients")+
   annotate(geom = "rect", xmin = as.Date("2021-01-01"),xmax = as.Date("2021-04-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
   annotate(geom = "rect", xmin = as.Date("2020-11-01"),xmax = as.Date("2020-12-01"),ymin = -Inf, ymax = Inf,fill="grey80", alpha=0.5)+
@@ -109,6 +93,5 @@ plot_pn_rate <- ggplot(df_gaps, aes(x=date))+
 
 ggsave(
    plot= plot_pn_rate,
-   filename="monthly_pn_rate_measures8wkcode.jpeg", path=here::here("output"),
+   filename="monthly_pn_rate_measures8wkcode_by_region.jpeg", path=here::here("output"),
 )
-
