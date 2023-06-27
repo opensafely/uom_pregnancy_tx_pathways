b0a1c40df1ba349ad84eb01ece0ea20cafcde8a8
djlhayes <dexter.hayes@manchester.ac.uk>
2023-06-27 15:32:06 +0100

    pn tables fixed

--- analysis/del_code_frequency.R
+++ analysis/del_code_frequency.R
@@ -1,9 +1,14 @@
 library('tidyverse')
 
-df_input <- read_csv(
-  here::here("output", "input_8wk.csv.gz"),
-  col_types = cols(patient_id = col_integer(),delivery_code_number = col_number())
-)
+# df_input <- read_csv(
+#   here::here("output", "input_8wk.csv.gz"),
+#   col_types = cols(patient_id = col_integer(),delivery_code_number = col_number())
+# )
+
+setwd(here::here("output", "pn8wk"))
+df_input<-list.files(pattern = "input", full.names = FALSE) %>% lapply(read.csv, stringsAsFactors=F) %>% bind_rows()
+
+col_types = cols(patient_id = col_integer(),delivery_code_number = col_number())
 
 df_input <- df_input %>% filter(delivery_code_number >0)
 
