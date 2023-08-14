a105e5bb5b139b65733519cd85b72d65a6735dd2
v1kki213 <victoria.palin@manchester.ac.uk>
2023-08-14 10:57:49 +0100

    updates del and pn code freq script and added action to yaml.

--- analysis/del_code_and_pn_code_frequency_by_patient.R
+++ analysis/del_code_and_pn_code_frequency_by_patient.R
@@ -78,3 +78,68 @@ table_pn_codes <- table_pn_codes[order(-table_pn_codes$Freq),]
 table_pn_codes$Freq <- ifelse(table_pn_codes$Freq < 10, "Redacted", table_pn_codes$Freq)
 write_csv(table_pn_codes, here::here("output","table_pn_codes_reviewed_overall.csv"))
 
+######### merge with code names
+## read in codelist csvs
+
+#load delivery codes
+df_del <- read_csv(
+  here::here("codelists", "user-VickiPalin-pregnancy_delivery_snomed_reviewed_2.csv"),
+  col_types = cols(code = col_number(),term = col_character())
+)
+
+#load pn codes
+df_pn <- read_csv(
+  here::here("codelists", "user-VickiPalin-pregnancy_postnatal_8wk_snomed_reviewed.csv"),
+  col_types = cols(code = col_number(),term = col_character())
+)
+
+
+
+### rename Var1 to code to match in codelist csv.
+
+#rename first column in frequency table to match
+colnames(table_del_codes)[1]<-"code"
+colnames(table_pn_codes)[1]<-"code"
+
+#merge by code number to return full description of codes which are in both files
+table_del_codes_merged<-merge(table_del_codes, df_del, by="code")
+table_del_codes_merged <- table_del_codes_merged[order(-table_del_codes_merged$Freq),]
+
+table_pn_codes_merged<-merge(table_pn_codes, df_pn, by="code")
+table_pn_codes_merged<- table_pn_codes_merged[order(-table_pn_codes_merged$Freq),]
+
+
+#######
+####### changes counts < 6 to "[REDACTED]"
+table_del_codes_mergedR <- table_del_codes_merged 
+table_del_codes_mergedR$Freq <- ifelse(table_del_codes_mergedR$Freq <= 7, "[REDACTED]", table_del_codes_mergedR$Freq)
+table_del_codes_mergedR$Freq <- as.numeric(table_del_codes_mergedR$Freq)
+
+table_pn_codes_mergedR <- table_pn_codes_merged 
+table_pn_codes_mergedR$Freq <- ifelse(table_pn_codes_mergedR$Freq <= 7, "[REDACTED]", table_pn_codes_mergedR$Freq)
+table_pn_codes_mergedR$Freq <- as.numeric(table_pn_codes_mergedR$Freq)
+
+
+#rounding to nearest 5
+table_del_codes_mergedR$Freq <- round(table_del_codes_mergedR$Freq/5)*5
+table_pn_codes_mergedR$Freq <- round(table_pn_codes_mergedR$Freq/5)*5
+
+
+### saving
+
+# already saved histograms and quantiles:
+# filename="del_code_histogram_reviewed_byPatient.jpeg"
+# "del_code_quantiles_by_patients.csv"
+# filename="pn_code_histogram_reviewed_byPatient.jpeg"
+# "pn_code_quantiles_by_patients.csv"
+
+## already saved overall codes without descriptions
+# "table_del_codes_reviewed_overall.csv"
+# "table_pn_codes_reviewed_overall.csv"
+
+## now save merged with names and redacted. 
+write_csv(table_del_codes_mergedR, here::here("output","table_del_codes_mergedR.csv"))
+write_csv(table_pn_codes_mergedR, here::here("output","table_pn_codes_mergedR.csv"))
+
+
+
