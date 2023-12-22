7911a1c9de50e2f9e23fbb34583f5aff4979c225
v1kki213 <victoria.palin@manchester.ac.uk>
2023-12-22 15:01:17 +0000

    added glm function

--- analysis/glm_for_no_pn_check.r
+++ analysis/glm_for_no_pn_check.r
@@ -1,6 +1,8 @@
 library('tidyverse')
-library("cowplot")
-library("dplyr")
+#library('lubridate')
+library('dplyr')
+#library("cowplot")
+
 setwd(here::here("output", "joined_8wk"))
 
 #2019
@@ -97,8 +99,55 @@ df_input <- df_input %>% dplyr::mutate(bmi_cat = case_when(is.na(bmi) ~ "Unknown
                                                  bmi>29.9 ~"Obese"))
 df_input$bmi_cat <- as.factor(df_input$bmi_cat)
 
+#### highrisk pregnancy
+## comorbidities. (Hypertension disorder before / during)
+## create charlson index
+df_input$cancer_comor<- ifelse(df_input$cancer_comor == 1L, 2L, 0L)
+df_input$cardiovascular_comor <- ifelse(df_input$cardiovascular_comor == 1L, 1L, 0L)
+df_input$chronic_obstructive_pulmonary_comor <- ifelse(df_input$chronic_obstructive_pulmonary_comor == 1L, 1L, 0)
+df_input$heart_failure_comor <- ifelse(df_input$heart_failure_comor == 1L, 1L, 0L)
+df_input$connective_tissue_comor <- ifelse(df_input$connective_tissue_comor == 1L, 1L, 0L)
+df_input$dementia_comor <- ifelse(df_input$dementia_comor == 1L, 1L, 0L)
+df_input$diabetes_comor <- ifelse(df_input$diabetes_comor == 1L, 1L, 0L)
+df_input$diabetes_complications_comor <- ifelse(df_input$diabetes_complications_comor == 1L, 2L, 0L)
+df_input$hemiplegia_comor <- ifelse(df_input$hemiplegia_comor == 1L, 2L, 0L)
+df_input$hiv_comor <- ifelse(df_input$hiv_comor == 1L, 6L, 0L)
+df_input$metastatic_cancer_comor <- ifelse(df_input$metastatic_cancer_comor == 1L, 6L, 0L)
+df_input$mild_liver_comor <- ifelse(df_input$mild_liver_comor == 1L, 1L, 0L)
+df_input$mod_severe_liver_comor <- ifelse(df_input$mod_severe_liver_comor == 1L, 3L, 0L)
+df_input$mod_severe_renal_comor <- ifelse(df_input$mod_severe_renal_comor == 1L, 2L, 0L)
+df_input$mi_comor <- ifelse(df_input$mi_comor == 1L, 1L, 0L)
+df_input$peptic_ulcer_comor <- ifelse(df_input$peptic_ulcer_comor == 1L, 1L, 0L)
+df_input$peripheral_vascular_comor <- ifelse(df_input$peripheral_vascular_comor == 1L, 1L, 0L)
+
+## total charlson for each patient 
+charlson=c("cancer_comor","cardiovascular_comor","chronic_obstructive_pulmonary_comor",
+           "heart_failure_comor","connective_tissue_comor", "dementia_comor",
+           "diabetes_comor","diabetes_complications_comor","hemiplegia_comor",
+           "hiv_comor","metastatic_cancer_comor" ,"mild_liver_comor",
+           "mod_severe_liver_comor", "mod_severe_renal_comor", "mi_comor",
+           "peptic_ulcer_comor" , "peripheral_vascular_comor" )
+
+df_input$charlson_score=rowSums(df_input[charlson])
+
+## Charlson - as a categorical group variable
+df_input <- df_input %>%
+  dplyr::mutate(charlsonGrp = case_when(charlson_score >0 & charlson_score <=2 ~ 2,
+                                        charlson_score >2 & charlson_score <=4 ~ 3,
+                                        charlson_score >4 & charlson_score <=6 ~ 4,
+                                        charlson_score >=7 ~ 5,
+                                        charlson_score == 0 ~ 1))
+
+df_input$charlsonGrp <- as.factor(df_input$charlsonGrp)
+df_input$charlsonGrp <- factor(df_input$charlsonGrp, 
+                               labels = c("zero", "low", "medium", "high", "very high"))
+
+
+
+
+
 mod2 <- glm(postnatal_8wk_code_present~age_cat+
-              ethnicity2+region+imd+bmi_cat, family=binomial(link=logit), data=df_input)
+              ethnicity2+region+imd+bmi_cat+charlsonGrp, family=binomial(link=logit), data=df_input)
 #summary(mod2)
 mod2df <- cbind(Estimate = coef(mod2), confint(mod2))
 mod2df.exp=round(exp(mod2df), digits = 2)
@@ -109,15 +158,13 @@ write_csv(mod2df.exp, here::here("output","mod2_fulladj_coef.csv"))
 
 #covid
 mod2_covid <- glm(postnatal_8wk_code_present~age_cat+
-                ethnicity+region+imd+bmi_cat+covid, family=binomial(link=logit), data=df_input)
+                ethnicity2+region+imd+bmi_cat+charlsonGrp+covid, family=binomial(link=logit), data=df_input)
 mod2covid_df <- cbind(Estimate = coef(mod2_covid), confint(mod2_covid))
 mod2covid_df.exp=round(exp(mod2covid_df), digits = 2)
 pval2<- coef(summary(mod2_covid))[,4]
-mod2df.exp<-cbind(mod2df.exp, pval2)
+mod2covid_df.exp<-cbind(mod2covid_df.exp, pval2)
+write_csv(mod2covid_df.exp, here::here("output","mod2_covid_fulladj_coef.csv"))
 
-
-
-write_csv(mod2_coviddf, here::here("output","mod2_covid_fulladj_coef.csv"))
 
 ### model on obs per patient ID
 ## take one row per patient
@@ -125,120 +172,75 @@ dfone_lats <- df_input %>% group_by(patient_id) %>%
   arrange(desc(delivery_code_date))%>%
   filter(row_number()==1)
 
-dfone_first <- df_input %>% group_by(patient_id) %>%
-  arrange((delivery_code_date))%>%
-  filter(row_number()==1)
+# dfone_first <- df_input %>% group_by(patient_id) %>%
+#   arrange((delivery_code_date))%>%
+  # filter(row_number()==1)
 
 ## sensitivity modelling on obs per person
-mod3first <- glm(postnatal_8wk_code_present~age_cat+
-              ethnicity+region+imd+bmi, family=binomial(link=logit), data=dfone_first)
+# mod3first <- glm(postnatal_8wk_code_present~age_cat+
+#               ethnicity+region+imd+bmi, family=binomial(link=logit), data=dfone_first)
 mod3last <- glm(postnatal_8wk_code_present~age_cat+
-                   ethnicity+region+imd+bmi, family=binomial(link=logit), data=dfone_lats)
+                   ethnicity2+region+imd+bmi_cat+charlsonGrp, family=binomial(link=logit), data=dfone_lats)
+
+mod3last_df <- cbind(Estimate = coef(mod3last), confint(mod3last))
+mod3last_df.exp=round(exp(mod3last_df), digits = 2)
+pval3last<- coef(summary(mod3last))[,4]
+mod3last_df.exp<-cbind(mod3last_df.exp, pval3last)
+write_csv(mod3last_df.exp, here::here("output","mod3_fulladj_last.csv"))
 
 
-# get coefs and CIs and plot
-library(broom)
-tidy1 <- as.data.frame(mod1$coefficients)
-tidy2 <- as.data.frame(mod2$coefficients)
-tidy2_covid <- as.data.frame(mod2_covid$coefficients)
-tidy3_first <- as.data.frame(mod3first$coefficients)
-tidy3_last <- as.data.frame(mod3last$coefficients)
+## plot
+library(forestplot)
 
-# Age adjusted model
-var.diag1 <- diag(vcov(mod1))
-tidy1$OR <- exp(tidy1$`mod1$coefficients`)
-tidy1$Oddstd <- sqrt((tidy1$OR^2)*var.diag1)
-tidy1$LOW <- tidy1$OR-(1.96*tidy1$Oddstd)
-tidy1$HIGH <- tidy1$OR+(1.96*tidy1$Oddstd)
-tidy1$names <- row.names(tidy1)
-write_csv(tidy1, here::here("output","mod1_tidy_OR_CI.csv"))
+# remove intercept
+mod2df.exp<- mod2df.exp[c(-1),]
+colnames(mod2df.exp)[2]<-"LOW"
+colnames(mod2df.exp)[3]<-"HIGH"
+# plottext2<- row.names(tidy2rd)
+# print(plottext2)
+blank_row <- data.frame(Estimate = "", LOW = "", HIGH = "",pval2 = "")
 
-# fully adjusted model 
-var.diag2 <- diag(vcov(mod2))
-tidy2$OR <- exp(tidy2$`mod2$coefficients`)
-tidy2$Oddstd <- sqrt((tidy2$OR^2)*var.diag2)
-tidy2$LOW <- tidy2$OR-(1.96*tidy2$Oddstd)
-tidy2$HIGH <- tidy2$OR+(1.96*tidy2$Oddstd)
-tidy2$names <- row.names(tidy2)
-write_csv(tidy2, here::here("output", "mod2_tidy_OR_CI.csv"))
+dfplot <- rbind(blank_row, mod2df.exp[1:6,],blank_row, blank_row, mod2df.exp[7:11,],
+                blank_row, blank_row, mod2df.exp[12:19,], blank_row,blank_row, mod2df.exp[20:24,],
+                blank_row, blank_row, mod2df.exp[25:27,],blank_row, blank_row, mod2df.exp[29:32,])
 
-# fully adjusted model plus covid time
-var.diag3 <- diag(vcov(mod2_covid))
-tidy2_covid$OR <- exp(tidy2_covid$`mod2_covid$coefficients`)
-tidy2_covid$Oddstd <- sqrt((tidy2_covid$OR^2)*var.diag3)
-tidy2_covid$LOW <- tidy2_covid$OR-(1.96*tidy2_covid$Oddstd)
-tidy2_covid$HIGH <- tidy2_covid$OR+(1.96*tidy2_covid$Oddstd)
-tidy2_covid$names <- row.names(tidy2_covid)
-write_csv(tidy2_covid, here::here("output","mod2_covid_tidy_OR_CI.csv"))
+plottext2 <- c("Age:", "14-19","20-24","30-34","35-39","40-44","45-49",
+               "","Ethnicity:",
+              # "Asian or Asian British", "Black or Black British", "Mixed", "Other",
+               "0", "2", "3", "4", "5",
+                "","Region:",
+               "East", "East Midlands", "North East", "North West", "South East",
+               "South West", "West Midlands","Yorkshire and The Humber",
+               "","IMD:",
+              "imd:0", "imd:1", "imd:2", "imd:3", "imd:4",
+               "", "BMI",
+              "Obese", "Overweight", "Underweight", "", "Charlson Gp",
+              "Low", "Medium", "High", "Very High")
 
-## sensitivity analysis ORs and CIs
-var.diag4 <- diag(vcov(mod3first))
-tidy3_first$OR <- exp(tidy3_first$`mod3first$coefficients`)
-tidy3_first$Oddstd <- sqrt((tidy3_first$OR^2)*var.diag4)
-tidy3_first$LOW <- tidy3_first$OR-(1.96*tidy3_first$Oddstd)
-tidy3_first$HIGH <- tidy3_first$OR+(1.96*tidy3_first$Oddstd)
-tidy3_first$names <- row.names(tidy3_first)
-write_csv(tidy3_first, here::here("output","mod3_first_OR_CI.csv"))
+dfplot <- dfplot %>% mutate_at(c(2:4), as.numeric)
 
-var.diag5 <- diag(vcov(mod3last))
-tidy3_last$OR <- exp(tidy3_last$`mod3last$coefficients`)
-tidy3_last$Oddstd <- sqrt((tidy3_last$OR^2)*var.diag5)
-tidy3_last$LOW <- tidy3_last$OR-(1.96*tidy3_last$Oddstd)
-tidy3_last$HIGH <- tidy3_last$OR+(1.96*tidy3_last$Oddstd)
-tidy3_last$names <- row.names(tidy3_last)
-write_csv(tidy3_last, here::here("output","mod3_last_OR_CI.csv"))
-
-#### filter to highrisk pregnancy (Hypertension disorder before / during)
+plot_mod2<-forestplot(labeltext=plottext2,
+                      mean = c(dfplot$Estimate), lower = c(dfplot$LOW), upper = c(dfplot$HIGH),
+                      ci.vertices = TRUE, ci.vertices.height = 0.2,
+                      col=fpColors(box= "royalblue", line="darkblue", zero="gray", hrz_lines="black"),
+                      title="Odds Ratio [95% CI]",
+                      txt_gp=fpTxtGp(label=gpar(cex=1.2), ticks=gpar(cex=1.1)),
+                      grid=TRUE,
+                      boxsize=0.3,
+                      zero=1,
+                      xticks=c(0.5, 0.75, 1.0,1.5,2.0), lwd.ci=1)
 
 
-# ## plot
-# library(forestplot)
-# 
-# # remove intercept
-# tidy2rd<- tidy2[c(-1,-12,-21),]
-# # plottext2<- row.names(tidy2rd)
-# # print(plottext2)
-# colnames(tidy2rd)[1]<-"mod2coefficients" 
-# blank_row <- data.frame(mod2coefficients = "", OR = "", Oddstd = "", LOW = "", HIGH= "")
-# 
-# dfplot <- rbind(blank_row, tidy2rd[1:6,],blank_row, blank_row, tidy2rd[7:10,],
-#                 blank_row, blank_row, tidy2rd[11:18,], blank_row,blank_row, tidy2rd[19:22,],
-#                 blank_row, tidy2rd[23,])
-# 
-# plottext2 <- c("Age:", "20-24","25-29","30-34","35-39","40-44","45-49",
-#                "","Ethnicity:",
-#                "Asian or Asian British", "Black or Black British", "Mixed", "Other",
-#                "","Region:",
-#                "East", "East Midlands", "North East", "North West", "South East",
-#                "South West", "West Midlands","Yorkshire and The Humber",
-#                "","IMD:",
-#                "imd:2", "imd:3", "imd:4", "imd:5",
-#                "", "BMI")
-# 
-# dfplot <- dfplot %>% mutate_at(c(1:5), as.numeric) 
-# 
-# plot_mod2<-forestplot(labeltext=plottext2, 
-#                       mean = c(dfplot$OR), lower = c(dfplot$LOW), upper = c(dfplot$HIGH),
-#                       ci.vertices = TRUE, ci.vertices.height = 0.2, 
-#                       col=fpColors(box= "royalblue", line="darkblue", zero="gray", hrz_lines="black"),
-#                       title="Odds Ratio [95% CI]", 
-#                       txt_gp=fpTxtGp(label=gpar(cex=1.2), ticks=gpar(cex=1.1)),
-#                       grid=TRUE, 
-#                       boxsize=0.3, 
-#                       zero=1, 
-#                       xticks=c(0.5, 0.75, 1.0,1.5,2.0), lwd.ci=1)
-# 
-# 
-# save_plot(here::here("output", "glm_fullAdjs.jpeg"),
-#           plot_mod2,
-#           base_width = 9, base_height = 10
-#           )
-# 
-# # save_forest(plot= plot_mod2,
-# #   filename="glm_fullAdjs.jpeg", path=here::here("output"),
-# # )
-# # 
-# 
-# ##compare with glmer() repeated delcodes per mum.. 
-# ##or select one row per mum as sensitivity.
+save_plot(here::here("output", "glm_fullAdjs.jpeg"),
+          plot_mod2,
+          base_width = 9, base_height = 10
+          )
+
+# save_forest(plot= plot_mod2,
+#   filename="glm_fullAdjs.jpeg", path=here::here("output"),
+# )
+#
+
+##compare with glmer() repeated delcodes per mum..
+##or select one row per mum as sensitivity.
 
