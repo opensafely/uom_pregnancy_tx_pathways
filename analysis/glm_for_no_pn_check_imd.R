023dd2bd4517d4378734e6a3601f0cfc338a5264
v1kki213 <victoria.palin@manchester.ac.uk>
2024-06-18 12:18:46 +0100

    trying to fix imd and region issues

--- analysis/glm_for_no_pn_check_imd.R
+++ analysis/glm_for_no_pn_check_imd.R
@@ -48,7 +48,7 @@ str(df_input$imd)
 # select variables for modelling
 # colnames(df_input)[3]<-"Age"
 # colnames(df_input)[7]<-"Region"
-colnames(df_input)[8]<-"IMD"
+# colnames(df_input)[8]<-"IMD"
 # colnames(df_input)[9]<-"BMI"
 #colnames(df_input)[40]<-"HBP"
 
@@ -56,17 +56,18 @@ colnames(df_input)[8]<-"IMD"
 # df_input <- df_input %>% filter(Ethnicity != "Unknown")
 # df_input$Ethnicity<-as.factor(df_input$Ethnicity)
 # df_input$Ethnicity<-droplevels(df_input$Ethnicity)
-df_input <- df_input %>% filter(IMD != "0")
-#df_input$IMD<-as.factor(df_input$IMD)
-df_input$IMD<-droplevels(df_input$IMD)
+df_input <- df_input %>% filter(imd != "0")
+df_input$imd<- factor(df_input$imd, levels= c("5", "0","1", "2", "3", "4"))
+df_input$imd <- relevel(df_input$imd, "5")# least deprived as reference
+df_input$imd<-droplevels(df_input$imd)
 
 
 ## for log
 print("dropped levels")
 ## for log
-levels(df_input$IMD)
-table(df_input$IMD)
-str(df_input$IMD)
+levels(df_input$imd)
+table(df_input$imd)
+str(df_input$imd)
 
 # df_input$charlsonGrp2 <- as.factor(df_input$charlsonGrp2)
 # df_input$hbp_pregnancy <- as.factor(df_input$hbp_pregnancy)
@@ -77,7 +78,7 @@ str(df_input$IMD)
 ###############
 #  short model  
 ## traditional glm()
-model_imd <- glm(postnatal_8wk_code_present ~ IMD, data = df_input, family = binomial(link = "logit"))
+model_imd <- glm(postnatal_8wk_code_present ~ imd, data = df_input, family = binomial(link = "logit"))
 
 # Extract coefficient estimates and exponentiate them
 fit_results <- tidy(model_imd, exponentiate = TRUE)
